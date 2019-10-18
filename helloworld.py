53f429e74bf3f21d2b1d0242c5d2dbcb1914e10e
oousterman <oousterman@yahoo.com>
2019-10-18 14:51:15 +0200

    Added GPIO code as well as vim settings

--- helloworld.py
+++ helloworld.py
@@ -4,15 +4,27 @@
 # PDP group 25
 
 from testclass import aux
+import RPi.GPIO as GPIO
+import time
 
 def say_hello():
-	print("Hello World")
-	print("It Worked!")
+    print("Hello World")
+    print("It Worked!")
 
 if __name__ == '__main__':
-	say_hello()
-        aux1 = aux(1,2,3)
-        aux1.modify_all()
-        aux1.print_all()
+    say_hello()
+    aux1 = aux(1,2,3)
+    aux1.modify_all()
+    aux1.print_all()
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18,GPIO.OUT)
+    print "LED on"
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    print "LED off"
+    GPIO.output(18,GPIO.LOW)
+
 
 
