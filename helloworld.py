f5667db94375d11d69a22754137695c7e70640d6
oousterman <oousterman@yahoo.com>
2019-10-24 10:42:14 +0200

    plotting examples and gpio testing

--- helloworld.py
+++ helloworld.py
@@ -17,14 +17,37 @@ if __name__ == '__main__':
     aux1.modify_all()
     aux1.print_all()
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18,GPIO.OUT)
-    print "LED on"
-    GPIO.output(18,GPIO.HIGH)
-    time.sleep(1)
-    print "LED off"
-    GPIO.output(18,GPIO.LOW)
+    p1 = 20 #high then low
+    p2 = 40 #pwm
+    p_in = 37 #analog in
 
+#chan_list = [11,12]
+# add as many channels as you want!
+# you can tuples instead i.e.:
+#   chan_list = (11,12)
+#GPIO.setup(chan_list, GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup((p1,p2),GPIO.OUT)
+    GPIO.setup(p_in, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    pwm1 = GPIO.PWM(p2, 1000)
+    pwm1.start(50)
+    print("pwm started")
+    GPIO.output(p1,GPIO.HIGH)
+    print("Pin on")
+    time.sleep(10)
+    GPIO.output(p1,GPIO.LOW)
+    print("Pin off")
+
+    print("begin analog input")
+    for i in range(100000):
+        temp = GPIO.input(p_in)
+        print (str(temp))
+        time.sleep(0.01)
+    print("analog input done")
+
+
+    GPIO.cleanup()
 
 
